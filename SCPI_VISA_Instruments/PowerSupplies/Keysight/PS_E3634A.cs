5063b5f8387f8a06a762024ed4d07d254de78c3c
Phillip Smelt <phils@borisch.com>
2024-09-26 15:30:02 -0400

    9/26/24

--- SCPI_VISA_Instruments/PowerSupplies/Keysight/PS_E3634A.cs
+++ SCPI_VISA_Instruments/PowerSupplies/Keysight/PS_E3634A.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net;
+using Agilent.CommandExpert.ScpiNet.AgE363x_1_7;
 using Agilent.CommandExpert.ScpiNet.AgE364xD_1_7;
 
 namespace ABT.Test.TestExecutive.SCPI_VISA_Instruments.PowerSupplies.Keysight {
 
-    public class PS_E3634A : Agilent.CommandExpert.ScpiNet.AgE364xD_1_7 {
-        public override String MODEL { get { return "E3634A"; } }
+    public class PS_E3634A : AgE363x {
+        public const String MODEL = "E3634A";
+        public Alias ID;
+
         public enum RANGE { P25V, P50V }
 
-        public PS_E3634A(SCPI_VISA_Instrument.Alias id, String description, String address, String className) { base(id, description, address, className); }
-
-        public RANGE RangeGet() { return (RANGE)Enum.Parse(typeof(RANGE), Query(":SOURce:VOLTage:RANGe?")); }
-
-        public void RangeSet(RANGE Range) { Command($":SOURce:VOLTage:RANGe {Range}"); }
+        public PS_E3634A(SCPI_VISA_Instrument.Alias id, String description, String address, String className) { base(address); }
+        
+        public AgE363x E3634A = new AgE363x("%GPIB0::3::INSTR");
     }
 }
