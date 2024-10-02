ffdacdf3170dec35ad95fbec41850b48e5a7079e
Phillip Smelt <phils@borisch.com>
2024-10-02 16:48:49 -0400

    10/2/24

--- Instruments/MSO_3014.cs
+++ Instruments/MSO_3014.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using Tektronix.Tkdpo2k3k4k.Interop;
-using Ivi.Driver.Interop;
 
 namespace ABT.Test.Instruments {
-    public class MSO_3014 {
-        Tkdpo2k3k4kClass Instance;
-        public MSO_3014() { Instance = new Tkdpo2k3k4kClass(); }
-        
+    public class MSO_3014 : Tkdpo2k3k4kClass {
+        public readonly String Address;
+        public MSO_3014(String Address) {
+            this.Address = Address;
+            Initialize(Address, false, true, "");
         }
+    }
 }
